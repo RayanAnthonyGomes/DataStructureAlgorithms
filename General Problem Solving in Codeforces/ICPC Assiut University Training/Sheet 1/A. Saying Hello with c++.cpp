825f1837531f4dba6e65b950c13aca91d8c0b196
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-03-27 15:52:53 +0600

    A

---
+++ General Problem Solving in Codeforces/ICPC Assiut University Training/Sheet 1/A. Saying Hello with c++.cpp
@@ -0,0 +1,25 @@
+// A. Say Hello With C++
+// time limit per test1 second
+// memory limit per test256 megabytes
+// Given a name S. Print "Hello, (name)" without parentheses.
+
+// Input
+// Only one line containing a string S.
+
+// Output
+// Print "Hello, " without quotes, then print name.
+
+// Example
+// InputCopy
+// programmer
+// OutputCopy
+// Hello, programmer
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    cout << "Hello, " << s << endl; 
+}
