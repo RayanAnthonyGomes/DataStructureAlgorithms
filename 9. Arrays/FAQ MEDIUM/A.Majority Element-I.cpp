466e90f60cc3c7becead27cbb8746d898390f12c
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-05-07 17:54:44 +0600

    better Majority Element-1

--- 9. Arrays/FAQ MEDIUM/A.Majority Element-I.cpp
+++ 9. Arrays/FAQ MEDIUM/A.Majority Element-I.cpp
@@ -85,7 +85,7 @@ class Solution {
 public:
     //BRUUTE FORCE SOLUTION:
     /// Function to find the majority element in an array
-    int majorityElement(vector<int>& nums) {
+    int majorityElementbrute(vector<int>& nums) {
         
         // Size of the given array
         int n = nums.size(); 
@@ -115,7 +115,30 @@ public:
     }
 
     //BEtter Solution
-    
+    int majorityElement(vector<int>& nums) {
+        
+        // Size of the given array
+        int n = nums.size();
+        
+        // Hash map to store element counts
+        unordered_map<int, int> mp;
+        
+        // Count occurrences of each element
+        for (int num : nums) {
+            mp[num]++;
+        }
+        
+        /* Iterate through the map to
+        find the majority element*/
+        for (auto& pair : mp) {
+            if (pair.second > n / 2) {
+                return pair.first;
+            }
+        }
+        
+        // Return -1 if no majority element is found
+        return -1;
+    }
 };
 
 int main() {
