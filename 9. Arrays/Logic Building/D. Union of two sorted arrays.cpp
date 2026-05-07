078984763bac5be24ce396a46757a5a046de787c
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-05-07 12:07:05 +0600

    Optimal Union Array

--- 9. Arrays/Logic Building/D. Union of two sorted arrays.cpp
+++ 9. Arrays/Logic Building/D. Union of two sorted arrays.cpp
@@ -75,7 +75,7 @@ using namespace std;
 class Solution {
 public:
     //bruteforce
-        vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> BRUTEunionArray(vector<int>& nums1, vector<int>& nums2) {
        // Using set for storing unique elements
         set<int> s; 
         vector<int> Union;
@@ -93,6 +93,53 @@ public:
         // Convert the set to vector to get the union
         for (int num : s) {
             Union.push_back(num);
+        }
+
+        return Union;
+    }
+    /*
+        Time Complexity of BRUTE
+        Time Complexity: O( (M+N)log(M+N) ), at max set can store M+N elements {when there are no common elements and elements in nums1 , nums2 are distntict}. So Inserting M+N th element takes log(M+N) time. Upon approximation across inserting all elements in worst, it would take O((M+N)log(M+N) time.
+
+Space Complexity: O(M+N), considering space of Union Array.
+    */
+
+
+    ///Optimal Approach
+        vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> Union; // Vector to store the union elements
+        int i = 0, j = 0;
+        int n = nums1.size();
+        int m = nums2.size();
+
+        while (i < n && j < m) {
+            // Case 1 and 2
+            if (nums1[i] <= nums2[j]){ 
+            
+            if (Union.size() == 0 || Union.back() != nums1[i])
+                Union.push_back(nums1[i]);
+                i++;
+            } 
+            
+            //case 3
+            else{
+                if (Union.size() == 0 || Union.back() != nums2[j])
+                Union.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        // If any element left in arr1
+        while (i < n){ 
+            if (Union.back() != nums1[i])
+                Union.push_back(nums1[i]);
+            i++;
+        }
+        // If any elements left in arr2
+        while (j < m){ 
+            if (Union.back() != nums2[j])
+                Union.push_back(nums2[j]);
+            j++;
         }
 
         return Union;
