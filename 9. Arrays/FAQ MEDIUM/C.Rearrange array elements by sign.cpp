26b65ca8991309214cd3c2ba4c9c4b546de6619c
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-05-08 12:04:20 +0600

    Rearrange Brute

--- 9. Arrays/FAQ MEDIUM/C.Rearrange array elements by sign.cpp
+++ 9. Arrays/FAQ MEDIUM/C.Rearrange array elements by sign.cpp
@@ -77,3 +77,51 @@ Interview Follow-ups
 How would you modify the algorithm to handle uneven counts of positives and negatives?
 If the counts are uneven: Fill the result array with as many alternating pairs as possible. Append the remaining elements (all positives or all negatives) to the end of the result array while preserving their order.
 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Function to rearrange the given array by signs
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Define 2 vectors, one for storing positive 
+        // and the other for negative elements of the array
+        vector<int> pos, neg;
+  
+        // Segregate the array into positives and negatives
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) pos.push_back(nums[i]);
+            else neg.push_back(nums[i]);
+        }
+  
+        // Positives on even indices, negatives on odd
+        for (int i = 0; i < n / 2; ++i) {
+            nums[2 * i] = pos[i];
+            nums[2 * i + 1] = neg[i];
+        }
+        
+        // Return the result
+        return nums;
+    }
+};
+
+int main() {
+    vector<int> A = {1, 2, -4, -5};
+    
+    // Create an instance of Solution class
+    Solution sol;
+    
+    // Get the rearranged array
+    vector<int> ans = sol.rearrangeArray(A);
+    
+    // Print the result
+    for (int num : ans) {
+        cout << num << " ";
+    }
+    
+    return 0;
+}
