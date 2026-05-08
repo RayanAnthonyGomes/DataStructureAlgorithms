9306e4ab03f192529f884cdb9859e536acd1e0fa
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-05-08 12:05:01 +0600

    Rearrange Optimal

--- 9. Arrays/FAQ MEDIUM/C.Rearrange array elements by sign.cpp
+++ 9. Arrays/FAQ MEDIUM/C.Rearrange array elements by sign.cpp
@@ -85,7 +85,7 @@ using namespace std;
 class Solution {
 public:
     // Function to rearrange the given array by signs
-    vector<int> rearrangeArray(vector<int>& nums) {
+    vector<int> BRUTErearrangeArray(vector<int>& nums) {
         int n = nums.size();
         
         // Define 2 vectors, one for storing positive 
@@ -106,6 +106,37 @@ public:
         
         // Return the result
         return nums;
+    }
+     vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Initialize a result vector of size n
+        vector<int> ans(n, 0); 
+        
+        // Initialize indices for positive and negative elements
+        int posIndex = 0, negIndex = 1;  
+        
+        // Traverse through each element in nums
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                
+                /* If current element is negative, place
+                it at the next odd index in ans*/
+                ans[negIndex] = nums[i];
+                
+                // Move to the next odd index
+                negIndex += 2;  
+                
+            } else {
+                ans[posIndex] = nums[i];
+
+                // Move to the next even index
+                posIndex += 2;  
+            }
+        }
+        
+        // Return the rearranged array
+        return ans;  
     }
 };
 
