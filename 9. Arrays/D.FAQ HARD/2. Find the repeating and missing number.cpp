6bed96c2d50d4c920b3194d01954b5415a9a7bba
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-05-14 01:37:30 +0600

    Learn  bit manipulation first and then attempt the xor

--- 9. Arrays/D.FAQ HARD/2. Find the repeating and missing number.cpp
+++ 9. Arrays/D.FAQ HARD/2. Find the repeating and missing number.cpp
@@ -229,6 +229,83 @@ Space Complexity: O(1) no extra space is used.
         // Return the results as {repeating, missing}
         return {(int)x, (int)y};
     }
+
+    //Theres another optimal solution but first you need to learn bit manipulation.
+    vector<int> findMissingRepeatingNumbersxor(vector<int>& nums) {
+        
+        // size of the array
+        int n = nums.size(); 
+
+        int xr = 0;
+
+        for (int i = 0; i < n; i++) {
+            // XOR of all elements in nums
+            xr = xr ^ nums[i]; 
+            
+            // XOR of numbers from 1 to n
+            xr = xr ^ (i + 1);  
+        }
+
+        // Get the rightmost set bit in xr
+        int number = (xr & ~(xr - 1));
+
+        //Group the numbers based on the differentiating bit
+        // Number that falls into the 0 group
+        int zero = 0; 
+        
+        // Number that falls into the 1 group
+        int one = 0;  
+
+        for (int i = 0; i < n; i++) {
+            
+            /* Check if nums[i] belongs to the 1 group
+            based on the differentiating bit*/
+            if ((nums[i] & number) != 0) {
+                
+                // XOR operation to find numbers in the 1 group
+                one = one ^ nums[i];
+                
+            } else {
+                // XOR operation to find numbers in the 0 group
+                zero = zero ^ nums[i]; 
+            }
+        }
+
+        // Group numbers from 1 to n based on differentiating bit
+        for (int i = 1; i <= n; i++) {
+            
+            /* Check if i belongs to the 1 group 
+            based on the differentiating bit*/
+            if ((i & number) != 0) {
+                
+                // XOR operation to find numbers in the 1 group
+                one = one ^ i; 
+                
+            } else {
+                // XOR operation to find numbers in the 0 group
+                zero = zero ^ i; 
+            }
+        }
+
+        // Count occurrences of zero in nums
+        int cnt = 0; 
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == zero) {
+                cnt++;
+            }
+        }
+
+        if (cnt == 2) {
+            /*zero is the repeating number,
+            one is the missing number*/
+            return {zero, one}; 
+        }
+        
+        /* one is the repeating number, 
+        zero is the missing number*/
+        return {one, zero}; 
+    }
 };
 
 int main() {
