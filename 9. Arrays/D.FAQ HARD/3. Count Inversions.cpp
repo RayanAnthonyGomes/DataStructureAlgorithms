726ce3a416abb49c6f8626c9f7ce303673c8b089
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-05-14 14:27:26 +0600

    Count Inversions Optimal Solution with merge sort

--- 9. Arrays/D.FAQ HARD/3. Count Inversions.cpp
+++ 9. Arrays/D.FAQ HARD/3. Count Inversions.cpp
@@ -134,6 +134,122 @@ Time Complexity: O(N2), for using 2 nested loops, where N is the size of the arr
     return cnt;
 
     }
+
+    //OPTIMAL SOLUTION::: (PREREQUISITE MERGE SORT)
+    /**
+     *Intuition 
+The optimal solution uses the concept of merge sort algorithm to count the inversion pairs in an array. Try to break down the problem as finding the count of inversion pairs in two sorted arrays. Finally, combine the count and return it.
+
+Approach 
+In order to solve this, keep two pointers i and j, where i will point to the first index of arr1[] and j will point to the first index of arr2[]. Now in each iteration, do the following:
+If arr1[i] less than or equal to arr2[j]: These two elements cannot be a pair and so we will move the pointer i to the next position.
+
+
+Why we moved the i pointer: We know, that the given arrays are sorted. So, all the elements after the pointer j, should be greater than a2[j]. Now, as a1[i] is smaller or equal to arr2[j], it is obvious that arr1[i] will be smaller or equal to all the elements after arr2[j]. We need a bigger value of arr1[i] to make a pair and so we move the i pointer to the next position i.e. next bigger value.
+If arr1[i] greater than arr2[j]: These two elements can be a pair and so we will update the count of pairs. Now, here, we should observe that as arr1[i] is greater than arr2[j], all the elements after arr1[i] will also be greater than arr2[j] and so, those elements will also make pair with arr2[j]. So, the number of pairs added will be n1-i (where n1 = size of arr1[ ]). Now, we will move the j pointer to the next position.
+
+
+
+The steps of the merge() function are the following:
+In the merge function, we will use a temp array to store the elements of the two sorted arrays after merging. Here, the range of the left array is low to mid and the range for the right half is mid+1 to high.
+Now we will take two pointers left and right, where left starts from low and right starts from mid+1.
+Using a while loop( while(left <= mid && right <= high)), we will select two elements, one from each half, and will consider the smallest one among the two. Then, we will insert the smallest element in the temp array.
+After that, the left-out elements in both halves will be copied as it is into the temp array. Now, we will just transfer the elements of the temp array to the range low to high in the original array
+
+Modifications in merge() and mergeSort():
+In order to count the number of pairs, we will keep a count variable, cnt, initialized to 0 beforehand inside the merge().
+While comparing a[left] and a[right] in the 3rd step of merge(), if a[left] > a[right], we will simply add this line: cnt += mid-left+1 (mid+1 = size of the left half)
+Now, we will return this cnt from merge() to mergeSort(). Inside mergeSort(), we will keep another counter variable that will store the final answer. With this cnt, we will add the answer returned from mergeSort() of the left half, mergeSort() of the right half, and merge(). Finally, we will return this cnt, as our answer from mergeSort(). 
+     * 
+
+     * Complexity Analysis 
+Time Complexity: O(NxlogN), where N is size of the given array. We are not changing the merge sort algorithm except by adding a variable to it. So, the time complexity is as same as the merge sort.
+
+Space Complexity: O(N), in the merge sort there is use a temporary array to store elements in sorted order.
+     * 
+     */
+    /* Merge function to count 
+    inversions and merge sorted halves*/
+    long long int merge(vector<int>& arr, int low, int mid, int high) {
+        
+        // Temporary array for merging
+        vector<int> temp;
+        
+        // Starting indices of left and right halves
+        int left = low;
+        int right = mid + 1;
+
+        // Count variable to count the pairs
+        long long int cnt = 0;
+
+        // Merge sorted halves into temp array
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                
+                temp.push_back(arr[left]);
+                left++;
+                
+            } 
+            else {
+                temp.push_back(arr[right]);
+                
+                // Count inversions
+                cnt += (mid - left + 1);
+                
+                right++;
+            }
+        }
+
+        // Copy remaining elements of left half
+        while (left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        // Copy remaining elements of right half
+        while (right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        /* Copy elements from temp 
+        array back to original array*/
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
+        
+        //return the count of inversions
+        return cnt;
+    }
+    
+    // Merge sort function to recursively sort and count inversions
+    long long int mergeSort(vector<int>& arr, int low, int high) {
+        long long int cnt = 0;
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            
+            // Sort left half
+            cnt += mergeSort(arr, low, mid);  
+            
+            // Sort right half
+            cnt += mergeSort(arr, mid + 1, high); 
+            
+            // Merge and count inversions
+            cnt += merge(arr, low, mid, high);  
+        }
+        return cnt;
+    }
+    
+//OPTIOMAL
+    // Function to find number of inversions in an array
+    long long int numberOfInversions(vector<int>& nums) {
+        
+        // Size of the array
+        int n = nums.size();
+
+        // Count the number of pairs
+        return mergeSort(nums, 0, n - 1);
+    }
 };
 
 int main() {
