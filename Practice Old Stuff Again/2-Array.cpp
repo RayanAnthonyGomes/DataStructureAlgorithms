f9bf65973a0eb6e2c722e835045b9e007994a79c
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-08-05 23:08:39 +0600

    Boilerplate?

---
+++ Practice Old Stuff Again/2-Array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution:
+    public:
+    {
+
+    }
+
+    private: {
+
+    }
+
+
+
+int main(){
+    int array [] = {1,23,5,123,56,1,2,5};
+    int n = array.size();
+
+    
+
+}
