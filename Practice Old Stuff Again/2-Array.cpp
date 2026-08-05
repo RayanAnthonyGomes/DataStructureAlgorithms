2fefe15d5583ddc7aeaf496ef8d056c14b445029
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-08-05 23:11:18 +0600

    Sum of array elems

--- Practice Old Stuff Again/2-Array.cpp
+++ Practice Old Stuff Again/2-Array.cpp
@@ -4,11 +4,16 @@ using namespace std;
 class Solution:
     public:
     {
+        //Print array elements sum
+        int sumArr(arr,n){
+            int sum  = 0;
+            for(int i = 0; i  < n;  i++){
+                sum+= arr[i];
+            }
+            return sum;
+        }
 
-    }
-
-    private: {
-
+        
     }
 
 
@@ -17,6 +22,12 @@ int main(){
     int array [] = {1,23,5,123,56,1,2,5};
     int n = array.size();
 
+    Solution  sol = sol.sumArr(array,n);
+
     
+
+
+
+
 
 }
