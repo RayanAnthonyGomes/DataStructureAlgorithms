a50ebe00d3ce3d4ecaad565ced617e822d5f2825
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-08-07 13:07:29 +0600

    Sum  of all array elements

--- Practice Old Stuff Again/2-Array.cpp
+++ Practice Old Stuff Again/2-Array.cpp
@@ -13,6 +13,14 @@ void print(vector<int> &nums){
     }
 }
 
+//Sum of all array elements
+int sumArr(vector<int> &nums){
+    int sum = 0;
+    for(int i = 0; i <  nums.size(); i++){
+        sum+= nums[i];
+    }
+    return sum;
+}
 
 
 
