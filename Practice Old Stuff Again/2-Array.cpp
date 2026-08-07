3316872134b2df048e8ce1975a2ac65fcaf15771
Rayan Anthony Gomes <rayangomes1329@gmail.com>
2026-08-07 13:46:31 +0600

    Array is  sorted in  asc order?

--- Practice Old Stuff Again/2-Array.cpp
+++ Practice Old Stuff Again/2-Array.cpp
@@ -1,57 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-//Print Array Elements
-void print(vector<int> &nums){
-    int size = nums.size();
-    cout << "Printing array: " << "\n";
-    for(int i =  0; i < size; i++){
-        cout << nums[i] << " ";
+    // Print Array Elements
+    void print(vector<int> &nums)
+    {
+        int size = nums.size();
+        cout << "Printing array: " << "\n";
+        for (int i = 0; i < size; i++)
+        {
+            cout << nums[i] << " ";
+        }
     }
-}
 
-//Sum of all array elements
-int sumArr(vector<int> &nums){
-    int sum = 0;
-    for(int i = 0; i <  nums.size(); i++){
-        sum+= nums[i];
+    // Sum of all array elements
+    int sumArr(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+        }
+        return sum;
     }
-    return sum;
-}
 
+    // Count  odd numbers  in  an array
 
-//Count  odd numbers  in  an array
-
-int cntOdd(vector<int> &nums){
-    int cnt = 0;
-    for(int i = 0; i < nums.size(); i++){
-        if(nums[i]%2==1) cnt++;
+    int cntOdd(vector<int> &nums)
+    {
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] % 2 == 1)
+                cnt++;
+        }
+        return cnt;
     }
-    return cnt;
-}
 
-
-//Reverse given array
-void revArr(vector<int> &nums){
-    int n = nums.size();
-    int left = 0; 
-    int right =  n-1;
-    while(left<right){
-        swap(nums[left], nums[right]);
-        left++, right--;
+    // Reverse given array
+    void revArr(vector<int> &nums)
+    {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        while (left < right)
+        {
+            swap(nums[left], nums[right]);
+            left++, right--;
+        }
     }
-    
-}
 
-// Linear Search Function
-    int linearSearch(vector<int>& nums, int target) {
+    // Check  if array is  sorted in asc  order
+    bool isSorted(vector<int> &nums)
+    {
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] < nums[i - i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Linear Search Function
+    int linearSearch(vector<int> &nums, int target)
+    {
         // Traverse the entire vector
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             // Check if current element is target
-            if (nums[i] == target) {
+            if (nums[i] == target)
+            {
                 // Return index
                 return i;
             }
@@ -61,7 +83,8 @@ void revArr(vector<int> &nums){
     }
 };
 
-int main() {
+int main()
+{
     vector<int> nums = {1, 2, 3, 4, 5};
     int target = 4;
 
@@ -69,21 +92,27 @@ int main() {
     Solution sol;
 
     // Call the linearSearch method
-    //int result = sol.linearSearch(nums, target);
+    // int result = sol.linearSearch(nums, target);
 
     // Print the result
-    //cout << result << endl;
+    // cout << result << endl;
 
     sol.print(nums); // This  works as printing array yay..
 
-    //Returning the count of odd numbers
+    // Returning the count of odd numbers
     int resultOdd = sol.cntOdd(nums);
-    cout << endl << "Count of Odd nums: " <<resultOdd;
+    cout << endl
+         << "Count of Odd nums: " << resultOdd;
 
-
-    //reversing  the array; c
+    // reversing  the array; c
     cout << endl;
-    sol.revArr(nums); cout  << "After reversing  the array: ";
+    sol.revArr(nums);
+    cout << "After reversing  the array: ";
     sol.print(nums);
+
+    // reversing array again to get to  normal version
+    sol.revArr(nums); /// now its in asc form lets check. with another algo
+    cout << endl << "Checking if the array  is sorted  in asc order: ";
+    cout << boolalpha <<(sol.isSorted(nums));
     return 0;
 }
